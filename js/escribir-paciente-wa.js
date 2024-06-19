e42c7790d16ba51220f29cdccfdd5fdc7fadbd9b
Luis Trujillo <lu.a.tru.sul@gmail.com>
2024-06-18 21:46:26 -0500

    Se crea una carpeta js. Se hace validacion con JS para escribir por WA al cliente. Se conecta inde.html con js

---
+++ js/escribir-paciente-wa.js
@@ -0,0 +1,29 @@
+let errorNroIngresado = 0;
+function escribirPacienteWhatsApp() {
+  let nroPaciente = document.getElementById("footer-paciente-wa").value;
+  var mensaje = "Hola, soy el Dr. Ramirez";
+  var encodedText = encodeURIComponent(mensaje);
+  var whatsappURL =
+    "whatsapp://send?phone=" + nroPaciente + "&text=" + encodedText;
+  if (nroPaciente.length == 12) {
+    window.location.href = whatsappURL;
+    document.getElementById("footer-paciente-wa").value = "";
+  } else if (nroPaciente == "") {
+    alert("Debe ingresar un número.");
+  } else if (
+    (nroPaciente.length < 12 || nroPaciente.length > 12) &&
+    errorNroIngresado < 2
+  ) {
+    errorNroIngresado++;
+    alert(
+      errorNroIngresado +
+        "Número incorrecto.\nIntento " +
+        errorNroIngresado +
+        " de 3"
+    );
+  } else {
+    alert("Intentos agotados\nSe borra el número ingresado.");
+    document.getElementById("footer-paciente-wa").value = "";
+    window.location.href = "index.html";
+  }
+}
